4e3235c7ea1133405d5f92fe4ba7c7559d14e61a
Caleb Hefner <hefnerca5@gmail.com>
2023-01-30 13:40:14 -0700

    Add project files.

---
+++ Mission04_cah264/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,11 @@
+﻿//This is my calculator app which pushes the grade to the class="display" div
+$("#btnSend").click(function () {
+        $("div").html(($("#gpaAssignments").val() * .5) +
+        ($("#gpaGroupProjects").val() * .1) +
+        ($("#gpaQuizzes").val() * .1) +
+        ($("#gpaMidterm").val() * .1) +
+        ($("#gpaFinal").val() * .1) +
+        ($("#gpaINTEX").val() * .1));
+
+    $('#display').append(str);
+})
