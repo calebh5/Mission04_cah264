8baae0654a9555c7471ba02bdad64883b1ca7dc6
Caleb Hefner <hefnerca5@gmail.com>
2023-01-30 19:12:06 -0700

    Edit Grade Calculator

--- Mission04_cah264/wwwroot/js/GradeCalculator.js
+++ Mission04_cah264/wwwroot/js/GradeCalculator.js
@@ -1,11 +1,51 @@
 ﻿//This is my calculator app which pushes the grade to the class="display" div
 $("#btnSend").click(function () {
-        $("div").html(($("#gpaAssignments").val() * .5) +
+        percentage = (($("#gpaAssignments").val() * .5) +
         ($("#gpaGroupProjects").val() * .1) +
         ($("#gpaQuizzes").val() * .1) +
         ($("#gpaMidterm").val() * .1) +
         ($("#gpaFinal").val() * .1) +
-        ($("#gpaINTEX").val() * .1));
+        ($("#gpaINTEX").val() * .1))
 
-    $('#display').append(str);
+    if (percentage >= 94) {
+        grade = "A"
+    }
+    else if (percentage >= 90) {
+        grade = "A-"
+    }
+    else if (percentage >= 87) {
+        grade = "B+"
+    }
+    else if (percentage >= 84) {
+        grade = "B"
+    }
+    else if (percentage >= 80) {
+        grade = "B-"
+    }
+    else if (percentage >= 77) {
+        grade = "C+"
+    }
+    else if (percentage >= 74) {
+        grade = "C"
+    }
+    else if (percentage >= 70) {
+        grade = "C-"
+    }
+    else if (percentage >= 67) {
+        grade = "D+"
+    }
+    else if (percentage >= 64) {
+        grade = "D"
+    }
+    else if (percentage >= 60) {
+        grade = "D-"
+    }
+    else {
+        grade = "E"
+    }
+
+    $("div").html(grade)
+
 })
+
+
